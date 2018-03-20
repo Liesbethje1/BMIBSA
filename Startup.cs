2bd449537bfe8305a607887e3089385a181c791e
Liesbethje1 <liesbethje1@hotmail.com>
2018-03-20 14:53:24 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,24 +1,34 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace bmibsaberekene
 {
-    public class Program
+    public class Startup
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+            Console.WriteLine("Geef je lengte in meter in");
+            double lengte = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geef je gewicht in");
+            double gewicht = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Je Bmi is " + berekenBMI(lengte, gewicht) + " Je BSA is " + berekenBSA(lengte, gewicht));
+            Console.ReadKey();
+        }
+        public static double berekenBMI(double lengte, double gewicht)
+        {
+            return (gewicht / (lengte * lengte));
 
-            host.Run();
+        }
+        public static double berekenBSA(double lengte, double gewicht)
+        {
+            return 0.024265 * Math.Pow((lengte * 100), 0.3964) * Math.Pow(gewicht, 0.5378);
         }
     }
 }
